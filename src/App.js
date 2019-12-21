a93a2c2a9436d6502432d5b76a02dc6c400d2f89
Ivan Ganev <iganevru@gmail.com>
2019-12-21 22:34:56 +0300

    lifecycles

--- src/App.js
+++ src/App.js
@@ -1,15 +1,42 @@
 import React, { Component } from 'react';
 import './App.css';
-import CardItem from './components/card-item';
+import Lifecycles from './components/lifecycles';
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      showChild: true,
+      text: '',
+    };
+  }
+
   render() {
     return (
       <div className="App">
-        <CardItem title="Card Item Title" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <button
+          onClick={() =>
+            this.setState(state => ({
+              showChild: !state.showChild,
+            }))
+          }
+        >
+          Toggle Lifecycles
+        </button>
+        <button
+          onClick={() =>
+            this.setState(state => ({
+              text: state.text + '_hello',
+            }))
+          }
+        >
+          Update Text
+        </button>
+        {this.state.showChild ? <Lifecycles text={this.state.text} /> : null}
       </div>
     );
   }
